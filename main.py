2d67eba7b39ee389c44855785f69aab016ea8398
gabchrisk <gabrielchrisk@gmail.com>
2025-08-27 21:33:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -219,4 +219,4 @@ async def transcribe(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000")
+    uvicorn.run(app, host="0.0.0.0", port=5000)
